db01cbc1b955ec1bac5c43dbc68b15526d2f4e67
ciruss <johanlaas4@gmail.com>
2018-05-21 14:45:43 +0300

    pageOpacity

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 window.onload = () => {
 
-  let setTimer = document.getElementById('setTimer')
-  
-  let timer = () => { 
-    let timerLength = document.getElementById('timerLength').value + '000'
+  let setTimer = document.getElementById('setTimer'),
+      timeLeft = document.getElementById('timeLeft')
+
+  const pageOpacity = (opacity) => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          {code: `document.body.style.opacity = "${opacity}"`})
+    })
+  }
+
+  const timer = () => { 
+    let timerLength = document.getElementById('timerLength').value + '000',
+        timeSet = timerLength
     
-    let timeRemaining = setInterval(() => {
+    const timeRemaining = setInterval(() => {
       timerLength -= 1000
       let minutes = Math.floor((timerLength % (1000 * 60 * 60)) / (1000 * 60))
-      let seconds = Math.floor((timerLength % (1000 * 60)) / (1000))
-
-      document.getElementById('timeLeft').innerHTML = 
+      let seconds = Math.floor((timerLength % (1000 * 60)) / (1000)) 
+      
+      timeLeft.innerHTML = 
         `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
 
-      if(timerLength <= 0) clearInterval(timeRemaining)
+      if(timerLength <= (timeSet - (timeSet / 4))) {
+        pageOpacity(0.75)
+      } if(timerLength <= (timeSet / 2)) {
+        pageOpacity(0.5)
+      } if(timerLength <= 0 + (timeSet / 4)) {
+        pageOpacity(0.25)
+      } if(timerLength <= 0) {
+        clearInterval(timeRemaining)
+        pageOpacity(0)
+      }
       
     }, 1000)
   }
-  setTimer.addEventListener("click", timer)
+  setTimer.addEventListener("click", timer)  
 }
